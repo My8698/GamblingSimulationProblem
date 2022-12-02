7cc904cba342cd810b44bc90654f563d4e077477
devraajmadhuri@gmail.com <devraajmadhuri@gmail.com>
2022-12-02 14:27:34 +0530

    Added:starting stake values per day

---
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+public class GamblingSimulation {
+    /***
+     *initialize starting amount and every
+     * bet per game for everyday
+     */
+    public static void startGambling() {
+        int startingAmount = 100;
+        int bet_Per_Game = 1;
+        System.out.println("starting amount=" + startingAmount);
+        System.out.println("bet per game=" + bet_Per_Game);
+    }
+    public static void main(String args[]) {
+        /**
+         * method call
+         */
+        startGambling();
+    }
+}
