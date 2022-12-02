3a40a81f7668dd4e779c965613970d65d51688f2
devraajmadhuri@gmail.com <devraajmadhuri@gmail.com>
2022-12-02 16:51:54 +0530

    Added:add conditions for gambler if win or lost

--- src/com/bridgelabz/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -1,27 +1,29 @@
 package com.bridgelabz;
-
 public class GamblingSimulation {
-    /***
-     *to decide either gambler win or loose the game
-     */
-    public void startGambling() {
-        int startingBetAmount = 100;
+     public void startGambling() {
+        int startingAmount=100;
         int bet_Per_Game = 1;
-        /**
-         * player either win or loose
-         */
-        int bet_result = (int) (Math.random() * 10) % 2;
-        if (bet_result == 0) {
-            startingBetAmount -= 1;
-            System.out.println("Gambler loose and left amount is= " + startingBetAmount);
-        } else {
-            startingBetAmount += 1;
-            System.out.println("Gambler win and left amount is=" + startingBetAmount);
-
+        /** Checking condition with while loop
+         *add conditions for gambler if win or lost
+         * 50% of stake,would resign for the day
+         * */
+         while ( startingAmount != 150 || startingAmount !=50) {
+            int bet_result = (int) (Math.random() * 10) % 2;
+            if (bet_result == 0) {
+                startingAmount -= bet_Per_Game;
+            } else {
+                startingAmount += bet_Per_Game;
+            }
+            if(startingAmount == 150) {
+                System.out.println("Win 50 % of the stake ");
+                break;}
+            else if (startingAmount == 50)
+            {
+                System.out.println("Lost 50 % of the stake");
+                break;}
         }
     }
-
-    public static void main(String[] args) {
+    public static void main (String[]args){
         GamblingSimulation play = new GamblingSimulation();
         play.startGambling();
     }
